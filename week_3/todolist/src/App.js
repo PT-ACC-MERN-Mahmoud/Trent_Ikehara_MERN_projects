c96089901cf19bb15342f1a4ae83e8537357846b
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2022-07-03 02:03:54 -0700

    update todolist

--- week_3/todolist/src/App.js
+++ week_3/todolist/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Form = () => {
+  const [input, setInput] = useState('');
+  const [list, setList] = useState([]);
+
+  function add(e) {
+    e.preventDefault();
+
+    if(input === '') return;
+
+    setList([...list, {
+      text: input,
+      completed: false
+    }]);
+
+    setInput('');
+  }
+
+  function remove(index) {
+    setList(list.filter((_item, i) => i !== index));
+  }
+
+  function Marked(index) {
+    const task = {
+      ...list[index]
+    };
+
+    task.completed = !task.completed;
+
+    setList([
+      ...list.slice(0, index),
+      task
+    ].concat(list.slice(index + 1)));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={add}>
+        <input
+          onChange={e => setInput(e.target.value)}
+          value={input}
+        />
+        <button>Add</button>
+      </form>
+      {list.map((item, i) => (
+        <div key={i}>
+          <span style={{ textDecoration: item.completed && 'line-through' }}>{item.text}</span>
+          <input
+            type="checkbox"
+            checked={item.completed}
+            onClick={() => Marked(i)}
+            readOnly
+          />
+          <button onClick={() => remove(i)}>Delete</button>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default Form;

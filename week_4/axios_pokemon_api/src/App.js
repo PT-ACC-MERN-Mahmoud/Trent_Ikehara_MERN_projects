c71d56e70b9f7dee8dcd8ffa91f12de4c83a3d24
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2022-07-03 02:33:59 -0700

    added axios

--- week_4/axios_pokemon_api/src/App.js
+++ week_4/axios_pokemon_api/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Pokemon = (props) => {
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+            .then(res => setData(res.data.results))
+            .catch(err => console.log(err))
+    }, []);  
+    return(
+        <div>
+            {data.map((pocket, i) =>
+              <div className='App' key={i}>
+                {pocket.name}
+              </div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default Pokemon;

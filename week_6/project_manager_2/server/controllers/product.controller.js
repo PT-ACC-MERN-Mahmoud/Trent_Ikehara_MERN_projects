614bb1d1f2159e40c476df0b98a39900de7c2df8
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2022-07-22 17:53:52 -0700

    Adding project_manager_2 to git

--- week_6/project_manager_2/server/controllers/product.controller.js
+++ week_6/project_manager_2/server/controllers/product.controller.js
@@ -5,5 +5,18 @@ module.exports = {
         Product.create(req.body) 
             .then((newProduct) => res.json(newProduct)) 
             .catch((err) => console.log(err));
+    },
+    
+    allProducts: (req, res) => {
+        Product.find({}) 
+            .then((allProducts) => res.json(allProducts))
+            .catch((err) => console.log(err));
+    },
+
+    oneProduct: (req, res) => {
+        Product.findOne({ _id: req.params.id })
+            .then((oneProduct) => res.json(oneProduct))
+            .catch((err) => console.log(err));
     }
+
 };
--- week_6/project_manager_2/server/routes/product.routes.js
+++ week_6/project_manager_2/server/routes/product.routes.js
@@ -2,4 +2,6 @@ const ProductController = require("../controllers/product.controller");
 
 module.exports = (app)=>{
     app.post("/api/products", ProductController.createProduct);
+    app.post("/api/products", ProductController.allProducts);
+    app.get("/api/products/:id", ProductController.oneProduct);
 }

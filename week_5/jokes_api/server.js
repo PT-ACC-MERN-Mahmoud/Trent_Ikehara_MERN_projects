80cec2db9104cc04b4b4dcf3e5e510d892e98c63
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2022-07-27 12:10:59 -0700

    adding files to git

---
+++ week_5/jokes_api/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+require("./server/config/mongoose.config");
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+require("./server/routes/jokes.routes")(app);
+
+app.listen(8000, () => console.log(`Express running on port ${port}`));
